1d2096602f5e06e5ce8e6a6b1032f11a80c13513
root <prathapjit@gmail.com>
2019-10-29 07:46:06 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3
+RUN pip install flask
+ADD  hello_world.py /
+EXPOSE 5000
+CMD ["python","hello_world.py"]
---
+++ hello_world.py
@@ -0,0 +1,15 @@
+from flask import Flask
+#url_for
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'manickam!'
+  
+  
+@app.route('/name')
+def hello_world1():
+    return 'prathap!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
