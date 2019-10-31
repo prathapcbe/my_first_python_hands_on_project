ed4c6f90158544af92ef5f727e17cef925252609
root <prathapjit@gmail.com>
2019-10-31 06:07:42 +0000

    modified-for-jenkins

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,17 @@
-from flask import Flask
+#from flask import Flask
 #url_for
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'manickam!'
+#@app.route('/')
+#def hello_world():
+ #   return 'manickam!'
   
   
-@app.route('/name')
-def hello_world1():
-    return 'prathap!'
+#@app.route('/name')
+#def hello_world1():
+ #   return 'prathap!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0')
+
+print ("hello prathap")
